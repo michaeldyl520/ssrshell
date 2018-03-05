96343621ca97ac925f5205543e6a79a0df6b6946
michaeldyl520 <michaeldyl520@gmail.com>
2018-03-05 10:47:58 +0800

    add ssr.sh

---
+++ ssr.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+paramsTotal=$#
+ssConfigPath="/etc/shadowsocks.json"
+ssrSsPath="/home/dyl/website/github/shadowsocksr/shadowsocks/local.py"
+if [ $paramsTotal -gt 1 ]; then
+	printf "Only one parameter can be entered.\n"
+	exit
+fi
+case $1 in
+start)
+	sudo python $ssrSsPath -c $ssConfigPath -d start
+	;;
+stop)
+	sudo python $ssrSsPath -c $ssConfigPath -d stop
+	;;
+*)
+	echo "Please enter the parameters start or stop"
+	;;
+esac
